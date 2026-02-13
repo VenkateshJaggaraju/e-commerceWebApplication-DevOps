03b0b2e5af4d380f219c4d4c887f1ecd87949956
VenkateshJaggaraju <venkateshjaggaraju2002@gmail.com>
2026-02-13 20:15:35 +0530

    added

---
+++ src/test/java/com/example/spring2/FlipzoneApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FlipzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
